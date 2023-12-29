cd75248861a48b128e20204b80ccf2c1e6e0685d
Немченко Роман Евгеньевич <rnemchenko@alfabank.ru>
2023-12-29 16:25:54 +0300

    fix(*): eslint

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'dist')})
-})
+  res.sendFile('index.html', { root: path.join(__dirname, 'dist') });
+});
 
 app.listen(PORT, function () {
   console.log(`Привет, мир! я на ${PORT} порте`);
